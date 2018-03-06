06283607dcf49811f9c1b8461fd02b326e5d77d7
albullington <albullington@gmail.com>
2018-03-05 18:19:04 -0800

    Finished version of recursion review (getElements + stringifyJSON).

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className,node = document.body) {
+  // pass in an extra parameter, node/element
+  // start by looking through the body of the document to see if there are matching elements
+  // iterate through the child nodes array
+  // then, use recursion to look through the child nodes
+  // hints: use body.document, element.childList, element.childNodes
+  var results = [];
+  
+  if (className !== undefined && node.classList.contains(className)) {
+    console.log(node);
+
+    results.push(node); 
+  }
+  
+  Array.from(node.children).forEach(function(child) {
+    // console.log('hi');
+    results = results.concat(getElementsByClassName(className,child))
+  });
+    // for (var i = 0; i < newArray.length; i++) {
+    //   results.push(getElementsByClassName(className,newArray[i]));
+    // }
+  // console.log(results);
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,47 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj) { 
+  var results = [];
+
+  //if (obj === null) {
+  //  return 'null'; 
+  //}
+  // if (obj === undefined) {
+  //   return undefined;  
+  // }
+    if (Array.isArray(obj)) {
+      for (var i = 0; i < obj.length; i++) {
+        results.push(stringifyJSON(obj[i]));
+      }
+      return '[' + results.concat() + ']';
+      
+    //} else if (typeof obj === 'function') {
+    //  return continue;
+
+    } else if (typeof obj === 'object' && obj !== undefined && obj !== null) {
+                                    //add !== null 
+      for (var key in obj) {
+        if (obj[key] === undefined || typeof obj[key] === "function") {
+          continue;
+        } else {
+        results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      } 
+    }
+    return '{' + results.concat() + '}';
+    //} else if (typeof obj === 'number' || typeof obj === 'boolean') {
+    //  return "" + obj; 
+
+      //take string as a if 
+      // the rest can be just '' + obj
+    } else if (typeof obj === 'string') {
+      return '"' + obj +'"';
+    } else {
+      return obj + "";
+    }
+  //object is not null
+  //solve for array (Array.isArray(obj))
+  //solve for object (typeof obj === 'object')
+  //solve for string (typeof obj === 'string')
+  //base case
 };
